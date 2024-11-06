f1cd8c7132b0f5986cd85d6f8dc09bfde38b6300
Frumosi <kkone.devops@gmail.com>
2024-11-06 06:32:05 -0500

    Adding temporary changes to Github

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -2,16 +2,10 @@ from django.contrib import admin
 from .models import CarMake, CarModel
 
 
-# Register your models here.
-
-# CarModelInline class
-
 
 class CarModelInline(admin.TabularInline):  # or use admin.StackedInline
     model = CarModel
     extra = 1
-
-# CarModelAdmin class
 
 
 class CarModelAdmin(admin.ModelAdmin):
@@ -20,15 +14,14 @@ class CarModelAdmin(admin.ModelAdmin):
     list_filter = ('year', 'car')                 # Fields to filter by in the sidebar
     ordering = ('-year',)                         # Ordering of the list (e.g., descending by year)
 
-# CarMakeAdmin class with CarModelInline
+
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
     list_display = ('name', 'description')       # Fields displayed in the CarMake list view
     search_fields = ('name', 'description')      # Search functionality for CarMake
     list_filter = ('name',)                      # Filter CarMakes by country
 
-# Register models here
-
+    
 # Registering models with their respective admins
 admin.site.register(CarMake)
 admin.site.register(CarModel)

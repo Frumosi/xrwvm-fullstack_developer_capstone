7fed46a65dc8dec238e7dc700f79d6ae6f3f4903
Frumosi <kkone.devops@gmail.com>
2024-11-06 10:45:38 -0500

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
 
     # path for logout
-    path(route='logout/', view=views.logout, name='logout'),
+    path(route="logout/", view=views.logout_request, name="logout"),
 
     # path for registration
     path(route='register/', view=views.registration, name='register'),

c742b09ad8eaf47884cd2a32e18dbfea1ba509e1
Frumosi <kkone.devops@gmail.com>
2024-11-06 07:13:26 -0500

    Adding temporary changes to Github

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,8 +1,8 @@
 from .models import CarMake, CarModel
 
 
+
 def initiate():
-    # initiate
     car_make_data = [
         {"name":"NISSAN", "description":"Great cars. Japanese technology"},
         {"name":"Mercedes", "description":"Great cars. German technology"},

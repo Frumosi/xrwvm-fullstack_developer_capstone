f1c4ab026f10a504816ac816c7a29ec70f69a9f8
Frumosi <kkone.devops@gmail.com>
2024-11-06 12:44:10 -0500

    Adding temporary changes to Github

--- server/database/app.js
+++ server/database/app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
 const  cors = require('cors');
-const app = express()
+const app = express();
 const port = 3030;
 const { MongoClient, ObjectId } = require('mongodb');
 const router = express.Router();
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -39,7 +39,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -104,22 +104,22 @@ app.get('/fetchDealer/:id', async (req, res) => {
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
-  data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
-
+    data = JSON.parse(req.body);
+    const documents = await Reviews.find().sort( { id: -1 } );
+    let new_id = documents[0]['id']+1;
+  
   const review = new Reviews({
-		"id": new_id,
-		"name": data['name'],
-		"dealership": data['dealership'],
-		"review": data['review'],
-		"purchase": data['purchase'],
-		"purchase_date": data['purchase_date'],
-		"car_make": data['car_make'],
-		"car_model": data['car_model'],
-		"car_year": data['car_year'],
-	});
-
+      "id": new_id,
+      "name": data.name, // Changed from data['name'] to data.name
+      "dealership": data.dealership, // Changed from data['dealership'] to data.dealership
+      "review": data.review, // Changed from data['review'] to data.review
+      "purchase": data.purchase, // Changed from data['purchase'] to data.purchase
+      "purchase_date": data.purchase_date, // Changed from data['purchase_date'] to data.purchase_date
+      "car_make": data.car_make, // Changed from data['car_make'] to data.car_make
+      "car_model": data.car_model, // Changed from data['car_model'] to data.car_model
+      "car_year": data.car_year, // Changed from data['car_year'] to data.car_year
+  });
+  
   try {
     const savedReview = await review.save();
     res.json(savedReview);

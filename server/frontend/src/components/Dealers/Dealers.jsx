9fea26b37378cb456e88f97792d60b454664dcdc
Frumosi <kkone.devops@gmail.com>
2024-11-07 11:16:57 -0500

    Adding temporary changes to Github

--- server/frontend/src/components/Dealers/Dealers.jsx
+++ server/frontend/src/components/Dealers/Dealers.jsx
@@ -4,24 +4,6 @@ import "../assets/style.css";
 import Header from '../Header/Header';
 import review_icon from "../assets/reviewicon.png"
 
-const handleInputChange = (event) => {
-const query = event.target.value;
-setSearchQuery(query);
-const filtered = originalDealers.filter(dealer =>
-    dealer.state.toLowerCase().includes(query.toLowerCase())
-);
-setDealersList(filtered);
-};
-
-
-const handleLostFocus = () => {
-if (!searchQuery) {
-    setDealersList(originalDealers);
-}
-}
-
-
-
 const Dealers = () => {
   const [dealersList, setDealersList] = useState([]);
   // let [state, setState] = useState("")
@@ -78,7 +60,13 @@ return(
       <th>Address</th>
       <th>Zip</th>
       <th>
-      <input type="text" placeholder="Search states..." onChange={handleInputChange} onBlur={handleLostFocus} value={searchQuery} /> 
+      <select name="state" id="state" onChange={(e) => filterDealers(e.target.value)}>
+      <option value="" selected disabled hidden>State</option>
+      <option value="All">All States</option>
+      {states.map(state => (
+          <option value={state}>{state}</option>
+      ))}
+      </select>        
 
       </th>
       {isLoggedIn ? (
